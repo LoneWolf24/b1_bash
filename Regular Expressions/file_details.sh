bcc82532bc6c093797f873bcd9a69203c8cc6778
Abdulaziz Kodirov <32646807+LoneWolf24@users.noreply.github.com>
2018-10-01 09:15:37 -0600

    Add files via upload

---
+++ Regular Expressions/file_details.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+clear
+#######################
+NONE='\033[00m'
+RED='\033[01;31m'
+GREEN='\033[01;32m'
+YELLOW='\033[01;33m'
+PURPLE='\033[01;35m'
+CYAN='\033[01;36m'
+WHITE='\033[01;37m'
+BOLD='\033[1m'
+UNDERLINE='\033[4m'
+BLINK='\033[5m'
+######################
+option='du -hsx';
+echo -e $GREEN
+cd ../
+ls -d *
+echo -e $NONE
+
+echo -n "Enter directory name:";
+read dir_name;
+cd $dir_name
+
+echo -e $GREEN
+$option *
+echo -e $NONE
+echo -n "Enter file name you wish to delete: ";
+read delete_file_name;
+echo -e $RED
+echo "File is removed"
+echo -e $NONE
+rm $delete_file_name;
+echo -e $GREEN
+$option *
+echo -e $NONE
